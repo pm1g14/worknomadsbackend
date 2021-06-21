90583e416926790b51a4b38be7cdc233eb6375bb
unknown <panamavr@hotmail.com>
2021-06-21 16:02:38 +0100

    skeleton spring project

---
+++ src/main/java/com/worknomads/worknomads/WorknomadsApplication.java
@@ -0,0 +1,13 @@
+package com.worknomads.worknomads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorknomadsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorknomadsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/worknomads/worknomads/WorknomadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worknomads.worknomads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorknomadsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
