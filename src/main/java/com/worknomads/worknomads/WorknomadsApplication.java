6ef2afc300dd245591b860be3c064248a312c892
arisf <aris.fanaras@mypinpad.group>
2021-08-20 22:50:47 +0100

    Added temp contacts2 repo.

--- src/main/java/com/worknomads/worknomads/WorknomadsApplication.java
+++ src/main/java/com/worknomads/worknomads/WorknomadsApplication.java
@@ -1,5 +1,6 @@
 package com.worknomads.worknomads;
 
+import com.worknomads.worknomads.dao.BusinessPartnerRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +10,4 @@ public class WorknomadsApplication {
     public static void main(String[] args) {
         SpringApplication.run(WorknomadsApplication.class, args);
     }
-
 }
